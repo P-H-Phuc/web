04945c8fe25d82b5aa25d2f35bde017fb44681f4
phucph <phucph@trobz.com>
2025-07-02 18:22:33 +0700

    [IMP] Update CONTRIBUTERS.md file

--- web_preview_pdf/static/src/many2many_binary/attachment_preview.js
+++ web_preview_pdf/static/src/many2many_binary/attachment_preview.js
@@ -1,22 +1,22 @@
 /** @odoo-module **/
 
-import { patch } from "@web/core/utils/patch";
-import { Many2ManyBinaryField } from "@web/views/fields/many2many_binary/many2many_binary_field";
+import {patch} from "@web/core/utils/patch";
+import {Many2ManyBinaryField} from "@web/views/fields/many2many_binary/many2many_binary_field";
 
-patch(
-    Many2ManyBinaryField.prototype,
-    "web_preview_pdf.AttachmentPreview",
-    {
-        isPdfDocument(file) {
-            return file.mimetype === "application/pdf";
-        },
+patch(Many2ManyBinaryField.prototype, "web_preview_pdf.AttachmentPreview", {
+    isPdfDocument(file) {
+        return file.mimetype === "application/pdf";
+    },
 
-        isImageDocument(file) {
-            return file.mimetype.startsWith("image/");
-        },
+    isImageDocument(file) {
+        return file.mimetype.startsWith("image/");
+    },
 
-        isPreviewable(file) {
-            return this.isPdfDocument(file) || this.isImageDocument(file) || this._super.apply(this, arguments);
-        }
-    }
-);
+    isPreviewable(file) {
+        return (
+            this.isPdfDocument(file) ||
+            this.isImageDocument(file) ||
+            this._super.apply(this, arguments)
+        );
+    },
+});
